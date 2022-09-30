557a6eb290de990500b7e53e6e5b51357b874e95
William-Thomas-ASR <William.Thomas@asr-group.com>
2022-09-30 09:55:42 -0400

    First version of get_pdf_text

---
+++ R/get_pdf_text.R
@@ -0,0 +1,27 @@
+
+#' get_pdf_page
+#'
+#' @param pdf Path to PDF containing Brickett Report
+#' @param page_num Page Number to be extracted
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_pdf_page <- function(pdf, page_num) {
+
+  page_num <- as.integer(page_num)
+
+  raw_text <- pdftools::pdf_text(pdf = pdf)
+
+  page_text <- raw_text[page_num]
+
+  page_text <- stringr::str_split(string = page_text, pattern =  '\\n')
+
+  page_text <- unlist(page_text)
+
+  page_text <- stringr::str_trim(string = page_text)
+
+  return(page_text)
+
+}
